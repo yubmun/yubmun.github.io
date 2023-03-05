9993d853b9169638a3654361afd20d22ceb5a6c6
yubmun <dlwnsduq23@gmail.com>
2023-03-05 22:08:05 +0900

    bunddle install

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "작성해보는 첫 게시글!",
+        "excerpt":"깃 블로그 어렵지 않다   Hello world, this is my first Jekyll blog post.   I hope you like it!  ","categories": ["practice"],
+        "tags": [],
+        "url": "/practice/first/",
+        "teaser": null
+      },{
+        "title": "카테고리 설정!",
+        "excerpt":"카테고리를 설정해봅니다.   카테고리 설정이 되겠쥬?  ","categories": ["practice"],
+        "tags": [],
+        "url": "/practice/practice/",
+        "teaser": null
+      }]
