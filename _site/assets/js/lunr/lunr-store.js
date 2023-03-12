3770b4159ab14b9a9af08b3ba0aa1a4c30f5335a
yubmun <dlwnsduq23@gmail.com>
2023-03-12 20:43:51 +0900

    server upload

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -10,4 +10,10 @@ var store = [{
         "tags": ["typescript"],
         "url": "/practice/practice/",
         "teaser": null
+      },{
+        "title": "브라우저 렌더링 과정",
+        "excerpt":"브라우저 렌더링 과정에 대해 우리가 작성한 코드들, HTML , CSS , JavaScript 코드들이 어떻게 렌더링이 되는지에 대해 공부했다. 개발자라면 알아야 하는 내용이며 어렵지 않게 이해할 수 있었다. 자, 그럼 단계에 대해서 알아보자. 1. HTML 파일을 파싱하고 DOM 트리를 구축한다. 여기서 DOM은, Document Object Model의 약자로 HTML 파일과 Script 언어들을 연결시켜줄...","categories": ["cs"],
+        "tags": ["cs"],
+        "url": "/cs/browserRendering/",
+        "teaser": null
       }]
